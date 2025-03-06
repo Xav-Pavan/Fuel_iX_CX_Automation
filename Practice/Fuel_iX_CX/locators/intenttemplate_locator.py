a267697c8c4f1639ee342db007241060c53b8da0
Xav-Pavan <puchha.pavan@telusinternational.com>
2025-03-06 14:55:26 +0530

    Delete Practice directory
    ok

--- Practice/Fuel_iX_CX/locators/intenttemplate_locator.py
+++
@@ -1,7 +0,0 @@
-class IntentTemplatePageLocators:
-    def __init__(self, page):
-        """Constructor to initialize login page locators."""
-        self.page = page
-        self.PROMPT_INPUT_BOX = self.page.get_by_test_id("promptinputbox")
-        self.PROMPT_OK_BUTTON = self.page.get_by_test_id("promptokbutton")
-        self.NODE_CREATION_PLUS_ICON = self.page.get_by_test_id("add-btn-flow")
--- Practice/PlayWright_Back_up/Locateors/loginPageLocators.py
+++
@@ -1,6 +0,0 @@
-class LoginPageLocators:
-    USERNAME_INPUT = "Enter email"
-    DOMAIN_INPUT = "Enter domain"
-    PASSWORD_INPUT = "Enter password"
-    LOGIN_BUTTON = "button"
-    DASHBOARDPAGEVALIDATION_TEXT = "//span[@title='Analytics & Reports']"
